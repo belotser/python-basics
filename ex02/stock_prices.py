8e3a9b1380dda02d969eb7702b3d6db86baa261c
belotser <forrlya@icloud.com>
2025-04-04 12:42:12 +0300

    implement dict searching

---
+++ ex02/stock_prices.py
@@ -0,0 +1,30 @@
+import sys
+
+def stock():
+	COMPANIES = {
+  'Apple': 'AAPL',
+  'Microsoft': 'MSFT',
+  'Netflix': 'NFLX',
+  'Tesla': 'TSLA',
+  'Nokia': 'NOK'
+  }
+
+	STOCKS = {
+  'AAPL': 287.73,
+  'MSFT': 173.79,
+  'NFLX': 416.90,
+  'TSLA': 724.88,
+  'NOK': 3.37
+  }
+
+	if (len(sys.argv) != 2):
+		sys.exit()
+	else:
+		try:
+			company = sys.argv[1].lower()
+			print(STOCKS[COMPANIES[company.capitalize()]])
+		except:
+			print("Unknown company")
+
+if __name__ == '__main__':
+	stock()
